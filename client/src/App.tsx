b06f4274133ffb98b7cc77a6061b6811729ca0aa
isaac <isaagarcia.developer.ca@gmail.com>
2023-05-06 11:20:29 -0300

    Theme Configuration

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,17 @@
+import { useMemo } from "react"
+import { createTheme } from '@mui/material/styles'
+import { themeSettings } from "./theme"
+import {CssBaseline, ThemeProvider} from "@mui/material"
+
 function App() {
+  const theme = useMemo(() => createTheme(themeSettings), [])
 
   return (
     <div className="app">
-      
+      <ThemeProvider theme={theme}>
+        <CssBaseline/>
+        Hi
+      </ThemeProvider>
     </div>
   )
 }
